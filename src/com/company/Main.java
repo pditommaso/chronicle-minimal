7c34b5ee0e49c10429c14e24e61bcc695d5ddf54
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-10-01 22:32:59 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.openhft.chronicle.map.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        final String pathname = "myfile.dat";
+
+        File file = new File(pathname);
+
+        ChronicleMap map = ChronicleMapBuilder
+                .of(Integer.class, CharSequence.class)
+                .file(file)
+                .entries(4000000)
+                .create();
+
+        map.put(1, "Hello");
+
+        map.close();
+    }
+}
