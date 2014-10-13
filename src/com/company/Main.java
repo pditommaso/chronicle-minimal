507445c84e85759f13db741b561ef7e93523d666
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2014-10-13 14:02:56 +0200

    Updated example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         final String pathname = "myfile.dat";
+        final long entries = args.length > 0 ? Long.parseLong(args[0]) : 1_000_000L; 
 
         File file = new File(pathname);
-
+        long begin = System.currentTimeMillis();
+        
+        System.out.printf("1. Begin using %5d entries\n", entries);
         ChronicleMap map = ChronicleMapBuilder
                 .of(Integer.class, CharSequence.class)
-                .entries(4_000_000_000L)
+                .entries(entries)
                 .create(file);
 
+        System.out.printf("2. After create: %5d ms\n", System.currentTimeMillis()-begin);
+        begin = System.currentTimeMillis();
+        
         map.put(1, "Hello");
-
+        System.out.printf("3. After put: %5d ms\n", System.currentTimeMillis()-begin);
+        begin = System.currentTimeMillis();
+        
         map.close();
+        System.out.printf("4. After close: %5d ms\n", System.currentTimeMillis()-begin);
+        
     }
 }
